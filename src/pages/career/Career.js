6ced69e8ada8f3095453fb5d5007858c1210a666
BaekDorazi <qorehgud@gmail.com>
2021-01-12 22:58:01 +0900

    UI수정 / Career 화면 내용 변경 및 색상 변경(아직 전부 x)

--- src/pages/career/Career.js
+++ src/pages/career/Career.js
@@ -8,63 +8,51 @@ class Career extends Component {
     this.state = {
       licenseArr: [
         {
-          acquisitionYear: "2014",
+          acquisitionYear: "2005",
           acquireLicense: [
             {
-              licenseName: "웹디자인기능사",
-              issuingAgency: "한국산업인력공단",
-              issueDate: "2014.04",
+              licenseName: "ITQ(한글, 파워포인트, 엑셀)",
+              issuingAgency: "한국생산성본부",
+              issueDate: "2005.05.07",
               isBr: false, //길어서 br로 내려야될때 false는 br태그 x
             },
             {
-              licenseName: "SOKI 국제 디자인 & 일러스트레이션 입상",
-              issuingAgency: "한국일러스트학회",
-              issueDate: "2014.01",
+              licenseName: "워드프로세서 2급",
+              issuingAgency: "대한상공회의소",
+              issueDate: "22005.11.23",
               isBr: false,
             },
           ],
         },
         {
-          acquisitionYear: "2009",
+          acquisitionYear: "2011",
           acquireLicense: [
             {
-              licenseName: "GTQ 포토샵",
-              issuingAgency: "한국생산성본부 KPC",
-              issueDate: "2009.10",
+              licenseName: "ICDL",
+              issuingAgency: "ICDL",
+              issueDate: "2011.10.10",
               isBr: false,
             },
+          ],
+        },
+        {
+          acquisitionYear: "2015",
+          acquireLicense: [
             {
-              licenseName: "정보기기운용기능사",
+              licenseName: "정보처리산업기사",
               issuingAgency: "한국산업인력공단",
-              issueDate: "2009.07",
-              isBr: false,
-            },
-          ],
-        },
-        {
-          acquisitionYear: "2008",
-          acquireLicense: [
-            {
-              licenseName: "정보기술자격 ITQ 한글파워포인트",
-              issuingAgency: "한국생산성본부 KPC",
-              issueDate: "2008.06",
+              issueDate: "2015.10.23",
               isBr: true,
             },
           ],
         },
         {
-          acquisitionYear: "2007",
+          acquisitionYear: "2016",
           acquireLicense: [
             {
-              licenseName: "워드프로세서",
-              issuingAgency: "대한상공회의소",
-              issueDate: "2007.08",
-              isBr: false,
-            },
-            {
-              licenseName: "정보기술자격 ITQ 아래한글",
-              issuingAgency: "한국생산성본부 KPC",
-              issueDate: "2007.07",
+              licenseName: "정보처리기사 ",
+              issuingAgency: "한국산업인력공단",
+              issueDate: "2016.07.15",
               isBr: true,
             },
           ],
@@ -73,45 +61,39 @@ class Career extends Component {
       skillArr: [
         {
           icon: "icon-adobephotoshop",
-          percentVal: 80,
+          percentVal: 90,
           color: "#00c8ff",
-          contents:
-            "Photoshop - 합성, 사진 편집 등의 2D 그래픽 작업을 비롯한 다양한 웹 시안 디자인 작업",
+          contents: "Java",
         },
         {
           icon: "icon-adobeillustrator",
-          percentVal: 50,
+          percentVal: 90,
           color: "#ff7c00",
-          contents:
-            "Illustrator - 디자인 목업, 배너, 플래카드 등과 아이콘, 로고, 심볼 등의 그래픽 디자인 작업",
+          contents: "Android",
         },
         {
           icon: "icon-adobeindesign",
-          percentVal: 80,
+          percentVal: 50,
           color: "#fd3f93",
-          contents:
-            "Indesign - 리플렛, 브로슈어, 카다로그, 책자 등의 인쇄물 편집 디자인 작업",
+          contents: "Kotlin",
         },
         {
           icon: "icon-adobeaftereffects",
           percentVal: 60,
           color: "#d291ff",
-          contents:
-            "After Effects - 영상 보정, 자막, 모션그래픽 등의 편집과 음악, 효과음 합성 작업",
+          contents: "Arduino",
         },
         {
           icon: "icon-adobepremiere",
-          percentVal: 80,
+          percentVal: 70,
           color: "#ea77ff",
-          contents:
-            "Premier - 영상 자르고 붙이는 등의 컷 편집과 음악, 효과음 합성 작업",
+          contents: "React",
         },
         {
           icon: "icon-embed2",
-          percentVal: 60,
+          percentVal: 70,
           color: "#000000",
-          contents:
-            "HTML+CSS - HTML로 구조화하고 CSS로 디자인을 적용하는 웹 디자인 및 퍼블리싱 작업",
+          contents: "HTML+CSS",
         },
       ], //skill 정보 json
     };
