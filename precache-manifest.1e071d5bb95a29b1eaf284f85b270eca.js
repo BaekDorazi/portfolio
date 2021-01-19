9249edc595dda5937c83c2dacb46d5ddcc73b3c3
BaekDorazi <qorehgud@gmail.com>
2021-01-19 23:57:00 +0900

    Updates

---
+++ precache-manifest.1e071d5bb95a29b1eaf284f85b270eca.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "360f9ab13e08c93adefa9585803c3ef3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0a75ec68684e48dbda16",
+    "url": "/portfolio/static/css/main.7004fb9a.chunk.css"
+  },
+  {
+    "revision": "0fd43acbccb237e92272",
+    "url": "/portfolio/static/js/2.b4c5ac1d.chunk.js"
+  },
+  {
+    "revision": "4a00a79d20439408526444321fa64abb",
+    "url": "/portfolio/static/js/2.b4c5ac1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a75ec68684e48dbda16",
+    "url": "/portfolio/static/js/main.9015f4fd.chunk.js"
+  },
+  {
+    "revision": "83f698d4fc7f12bbc12e",
+    "url": "/portfolio/static/js/runtime-main.828378b2.js"
+  },
+  {
+    "revision": "f9179daaf800c5275c5e76eaeef46e69",
+    "url": "/portfolio/static/media/homepage_01.f9179daa.png"
+  },
+  {
+    "revision": "b3aea7dc19d6414b175dd73e2df96a73",
+    "url": "/portfolio/static/media/profile.b3aea7dc.jpg"
+  },
+  {
+    "revision": "42f20b8b55c3b842fbf978e3d8ecba97",
+    "url": "/portfolio/static/media/screensaver_01.42f20b8b.png"
+  },
+  {
+    "revision": "d46330a69cd74b1ab0b52f1b08fdd7db",
+    "url": "/portfolio/static/media/webbanner_01.d46330a6.png"
+  }
+]);
