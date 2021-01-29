5ec4ed7c51dc615a564a7f5a661ba0516d88e48e
BaekDorazi <qorehgud@gmail.com>
2021-01-29 22:37:28 +0900

    Updates

---
+++ precache-manifest.766f2154a89e1eefe5006b5d0dfae08a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5d6b0f161852234268ec68e9b422f6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "21a641167645aec778d4",
+    "url": "/portfolio/static/css/main.d895b5fe.chunk.css"
+  },
+  {
+    "revision": "072120f728f2c4593380",
+    "url": "/portfolio/static/js/2.79791109.chunk.js"
+  },
+  {
+    "revision": "4a00a79d20439408526444321fa64abb",
+    "url": "/portfolio/static/js/2.79791109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21a641167645aec778d4",
+    "url": "/portfolio/static/js/main.a43e309a.chunk.js"
+  },
+  {
+    "revision": "83f698d4fc7f12bbc12e",
+    "url": "/portfolio/static/js/runtime-main.828378b2.js"
+  },
+  {
+    "revision": "01ffe7d2d1366098d7fe287d0e88ac9c",
+    "url": "/portfolio/static/media/portfolio_img01.01ffe7d2.png"
+  },
+  {
+    "revision": "bc54e6fa9d907ec842e1489007593df4",
+    "url": "/portfolio/static/media/portfolio_img02.bc54e6fa.png"
+  },
+  {
+    "revision": "fdde3d1962dad96e3f4e862fe6184375",
+    "url": "/portfolio/static/media/portfolio_img03.fdde3d19.png"
+  },
+  {
+    "revision": "1099df0b1d305b73e6bfeb241582cfce",
+    "url": "/portfolio/static/media/portfolio_img04.1099df0b.png"
+  },
+  {
+    "revision": "b220812d45ffd2e6aa1929b2cc8a7152",
+    "url": "/portfolio/static/media/portfolio_img05.b220812d.png"
+  },
+  {
+    "revision": "ee0423beb490162f50a20d1b0569bbdb",
+    "url": "/portfolio/static/media/portfolio_img06.ee0423be.png"
+  },
+  {
+    "revision": "fab1fae6598b4bd5455f06f162bc41e9",
+    "url": "/portfolio/static/media/portfolio_img07.fab1fae6.png"
+  },
+  {
+    "revision": "b3aea7dc19d6414b175dd73e2df96a73",
+    "url": "/portfolio/static/media/profile.b3aea7dc.jpg"
+  }
+]);
