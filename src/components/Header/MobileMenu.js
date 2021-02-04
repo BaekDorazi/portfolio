467decfa0830441a766186e17f506fbb691bfe3f
BaekDorazi <qorehgud@gmail.com>
2021-02-04 22:01:23 +0900

    UI수정 / 햄버거 메뉴 누르면 메뉴 보여주도록(css 적용 x)

--- src/components/Header/MobileMenu.js
+++ src/components/Header/MobileMenu.js
@@ -1,16 +1,41 @@
 import React, { Component } from "react";
+import ReactFullpage from "@fullpage/react-fullpage";
 
 class MobileMenu extends Component {
   render() {
     return (
-      <div>
+      <div className="mobile-menu-container">
         <input type="checkbox" id="menuicon" />
         <label for="menuicon">
           <span />
           <span />
           <span />
         </label>
-        <div class="sidebar"></div>
+        <div class="sidebar">
+          <ReactFullpage
+            render={({ state, fullpageApi }) => {
+              return (
+                <div className="menu">
+                  <div className="" onClick={() => fullpageApi.moveTo(1, 0)}>
+                    Intro
+                  </div>
+                  <div className="" onClick={() => fullpageApi.moveTo(2, 0)}>
+                    Profile
+                  </div>
+                  <div className="" onClick={() => fullpageApi.moveTo(3, 0)}>
+                    Career
+                  </div>
+                  <div className="" onClick={() => fullpageApi.moveTo(4, 0)}>
+                    Portfolio
+                  </div>
+                  <div className="" onClick={() => fullpageApi.moveTo(5, 0)}>
+                    Contact
+                  </div>
+                </div>
+              );
+            }}
+          />
+        </div>
       </div>
     );
   }
