ecfb129406255b4fbbf457e9467bdcfc46f42759
BaekDorazi <qorehgud@gmail.com>
2021-02-10 17:57:00 +0900

    UI수정 / 포트폴리오 상세보기에서 반응형코드 추가시키는중(중간커밋)

--- src/components/PortfolioModal/PortfolioModal.js
+++ src/components/PortfolioModal/PortfolioModal.js
@@ -27,24 +27,51 @@ class PortfolioModal extends Component {
         <Modal contentClassName="custom-modal" isOpen={show} toggle={toggle}>
           <ModalHeader toggle={toggle}>{portfolioInfo.title}</ModalHeader>
           <ModalBody className={portfolioInfo.fontColor}>
-            <img src={portfolioInfo.image} width="100%" height="auto" />
-            <span className="close-btn" onClick={toggle}>
-              X
-            </span>
-            <div className="sub-title-container">{portfolioInfo.subTitle}</div>
-            <div className="title-container">{portfolioInfo.title}</div>
-            <div className="contents-container">{contentsComp}</div>
-            <div className="period-container">
-              <div className="container-title">PERIOD</div>
-              <div className="container-contents">{portfolioInfo.period}</div>
+            <div className="web-display">
+              <img src={portfolioInfo.image} width="100%" height="auto" />
+              <span className="close-btn" onClick={toggle}>
+                X
+              </span>
+              <div className="sub-title-container">
+                {portfolioInfo.subTitle}
+              </div>
+              <div className="title-container">{portfolioInfo.title}</div>
+              <div className="contents-container">{contentsComp}</div>
+              <div className="period-container">
+                <div className="container-title">PERIOD</div>
+                <div className="container-contents">{portfolioInfo.period}</div>
+              </div>
+              <div className="ide-container">
+                <div className="container-title">IDE</div>
+                <div className="container-contents">{portfolioInfo.ide}</div>
+              </div>
+              <div className="join-container">
+                <div className="container-title">JOIN</div>
+                <div className="container-contents">
+                  {portfolioInfo.join} 명
+                </div>
+              </div>
             </div>
-            <div className="ide-container">
-              <div className="container-title">IDE</div>
-              <div className="container-contents">{portfolioInfo.ide}</div>
-            </div>
-            <div className="join-container">
-              <div className="container-title">JOIN</div>
-              <div className="container-contents">{portfolioInfo.join} 명</div>
+            <div className="mobile-display">
+              <img src={portfolioInfo.image} width="100%" height="auto" />
+              <div className="sub-title-container">
+                {portfolioInfo.subTitle}
+              </div>
+              <div className="period-container">
+                <div className="container-title">PERIOD</div>
+                <div className="container-contents">{portfolioInfo.period}</div>
+              </div>
+              <div className="ide-container">
+                <div className="container-title">IDE</div>
+                <div className="container-contents">{portfolioInfo.ide}</div>
+              </div>
+              <div className="join-container">
+                <div className="container-title">JOIN</div>
+                <div className="container-contents">
+                  {portfolioInfo.join} 명
+                </div>
+              </div>
+              <div className="contents-container">{contentsComp}</div>
             </div>
           </ModalBody>
         </Modal>
