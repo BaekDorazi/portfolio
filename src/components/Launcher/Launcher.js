6337a5af0a8c8c08ee0d9a6c3ccc8378ef2167dc
BaekDorazi <qorehgud@gmail.com>
2021-03-26 22:52:17 +0900

    기능수정 / Launcher 클래스 컴포넌트 -> 함수형 컴포넌트로 변경

--- src/components/Launcher/Launcher.js
+++ src/components/Launcher/Launcher.js
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Launcher extends Component {
-  render() {
-    return (
-      <div className="launcher-container">
-        <div className="loading-container">
-          <div className="bubble1 bubble"></div>
-          <div className="bubble2 bubble"></div>
-          <div className="bubble3 bubble"></div>
-        </div>
-        <div className="text">Loading</div>
+const Launcher = () => {
+  return (
+    <div className="launcher-container">
+      <div className="loading-container">
+        <div className="bubble1 bubble" />
+        <div className="bubble2 bubble" />
+        <div className="bubble3 bubble" />
       </div>
-    );
-  }
-}
+      <div className="text">Loading</div>
+    </div>
+  );
+};
 
 export default Launcher;
