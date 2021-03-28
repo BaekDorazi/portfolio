b2bfc8863b890613de09ffdcda2228ab4bf0466b
BaekDorazi <qorehgud@gmail.com>
2021-03-28 21:31:50 +0900

    기능수정 / SideName 클래스 컴포넌트 -> 함수형 컴포넌트로 수정

--- src/components/Header/SideNav.js
+++ src/components/Header/SideNav.js
@@ -1,16 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 
-class SideNav extends Component {
-  render() {
-    const { showPageName } = this.props;
+const SideNav = (props) => {
+  const { showPageName } = props;
 
-    return (
-      <div class="sidenav-container">
-        <div class="left-text">Developer_BaekDoHyung</div>
-        <div class="right-text">{showPageName}</div>
-      </div>
-    );
-  }
-}
+  return (
+    <div class="sidenav-container">
+      <div class="left-text">Developer_BaekDoHyung</div>
+      <div class="right-text">{showPageName}</div>
+    </div>
+  );
+};
 
 export default SideNav;
