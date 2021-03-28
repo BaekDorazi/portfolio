089d2d091ead94077d70e6663303b44627a4e811
BaekDorazi <qorehgud@gmail.com>
2021-03-28 21:26:04 +0900

    기능수정 / Logo 클래스 컴포넌트 -> 함수형 컴포넌트로 수정

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,17 +1,15 @@
 import React, { Component } from "react";
 import moment from "moment";
 
-class Logo extends Component {
-  render() {
-    return (
-      <div>
-        <div className="logo-container">
-          <div className="logo">BAEKDOHYUNG</div>
-          <div className="date">{moment().format("YYYY.MM")}</div>
-        </div>
+const Logo = () => {
+  return (
+    <div>
+      <div className="logo-container">
+        <div className="logo">BAEKDOHYUNG</div>
+        <div className="date">{moment().format("YYYY.MM")}</div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Logo;
