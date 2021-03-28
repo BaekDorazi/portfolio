2194c77c910141504b424db23ce048bfb2f9fcb1
BaekDorazi <qorehgud@gmail.com>
2021-03-28 21:33:33 +0900

    기능수정 / MobileMenu 클래스 컴포넌트 -> 함수형 컴포넌트로 수정

--- src/components/Header/MobileMenu.js
+++ src/components/Header/MobileMenu.js
@@ -1,59 +1,57 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactFullpage from "@fullpage/react-fullpage";
 
-class MobileMenu extends Component {
-  render() {
-    return (
-      <div className="mobile-menu-container">
-        <input type="checkbox" id="menuicon" />
-        <label for="menuicon">
-          <span />
-          <span />
-          <span />
-        </label>
-        <div class="sidebar">
-          <ReactFullpage
-            render={({ state, fullpageApi }) => {
-              return (
-                <div className="side-menu-container">
-                  <div
-                    className="menu-text"
-                    onClick={() => fullpageApi.moveTo(1, 0)}
-                  >
-                    INTRO
-                  </div>
-                  <div
-                    className="menu-text"
-                    onClick={() => fullpageApi.moveTo(2, 0)}
-                  >
-                    PROFILE
-                  </div>
-                  <div
-                    className="menu-text"
-                    onClick={() => fullpageApi.moveTo(3, 0)}
-                  >
-                    CAREER
-                  </div>
-                  <div
-                    className="menu-text"
-                    onClick={() => fullpageApi.moveTo(4, 0)}
-                  >
-                    PORTFOLIO
-                  </div>
-                  <div
-                    className="menu-text"
-                    onClick={() => fullpageApi.moveTo(5, 0)}
-                  >
-                    CONTACT
-                  </div>
+const MobileMenu = () => {
+  return (
+    <div className="mobile-menu-container">
+      <input type="checkbox" id="menuicon" />
+      <label for="menuicon">
+        <span />
+        <span />
+        <span />
+      </label>
+      <div class="sidebar">
+        <ReactFullpage
+          render={({ state, fullpageApi }) => {
+            return (
+              <div className="side-menu-container">
+                <div
+                  className="menu-text"
+                  onClick={() => fullpageApi.moveTo(1, 0)}
+                >
+                  INTRO
                 </div>
-              );
-            }}
-          />
-        </div>
+                <div
+                  className="menu-text"
+                  onClick={() => fullpageApi.moveTo(2, 0)}
+                >
+                  PROFILE
+                </div>
+                <div
+                  className="menu-text"
+                  onClick={() => fullpageApi.moveTo(3, 0)}
+                >
+                  CAREER
+                </div>
+                <div
+                  className="menu-text"
+                  onClick={() => fullpageApi.moveTo(4, 0)}
+                >
+                  PORTFOLIO
+                </div>
+                <div
+                  className="menu-text"
+                  onClick={() => fullpageApi.moveTo(5, 0)}
+                >
+                  CONTACT
+                </div>
+              </div>
+            );
+          }}
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default MobileMenu;
